e6e0fccba6dacee2cc09c2c2624a668ee677c3fc
William Perkins <william.perkins@pnnl.gov>
2021-01-25 12:03:15 -0800

    Add a (untested) storage based on functions

--- storage_factory_module.f90
+++ storage_factory_module.f90
@@ -15,63 +15,78 @@ MODULE storage_factory_module
   TYPE, PUBLIC :: storage_factory
    CONTAINS
      PROCEDURE, PRIVATE :: generate_simple => storage_factory_generate_simple
+     PROCEDURE, PRIVATE :: generate_function => storage_factory_generate_function
      PROCEDURE :: generate => storage_factory_generate
      
   END type storage_factory
+
+  PRIVATE json_get_double
   
 CONTAINS
 
   ! ----------------------------------------------------------------
-  !  FUNCTION storage_factory_generate_simple
+  !  FUNCTION json_get_double
   ! ----------------------------------------------------------------
-  FUNCTION storage_factory_generate_simple(this, svalue) RESULT (sptr)
+  FUNCTION json_get_double(json, svalue, fld, routine, x) RESULT(ierr)
+
+    IMPLICIT NONE
+    
+    INTEGER :: ierr
+    TYPE (json_core), INTENT(INOUT) :: json
+    TYPE (json_value), POINTER, INTENT(IN) :: svalue
+    CHARACTER (LEN=*), INTENT(IN) :: fld, routine
+    DOUBLE PRECISION, INTENT(OUT) :: x
+    
+    CHARACTER (LEN=256) :: msg
+    LOGICAL :: found
+
+    x = -9999.0
+    ierr = 0
+    CALL json%get(svalue, fld, x, found)
+    IF (json%failed()) THEN
+       WRITE(msg, *) TRIM(routine), ': JSON error looking for ', TRIM(fld)
+       CALL error_message(msg, fatal=.FALSE.)
+       ierr = ierr + 1
+    ELSE IF ( (.NOT. found)) THEN
+       WRITE(msg, *) TRIM(routine), ': required value "', TRIM(fld), '" not found'
+       CALL error_message(msg, fatal=.FALSE.)
+       ierr = ierr + 1
+    END IF
+
+  END FUNCTION json_get_double
+
+
+  ! ----------------------------------------------------------------
+  !  FUNCTION storage_factory_generate_simple
+  !
+  ! An example simple storage JSON:
+  !
+  !      "Storage" : {
+  !        "type" : "simple",
+  !        "Area" : 1000000.0,
+  !        "BottomElevation" : 4.0,
+  !    }
+  !
+  ! ----------------------------------------------------------------
+  FUNCTION storage_factory_generate_simple(this, json, svalue) RESULT (sptr)
 
     IMPLICIT NONE
     TYPE (storage_ptr) :: sptr
     CLASS (storage_factory), INTENT(INOUT) :: this
+    TYPE (json_core), INTENT(INOUT) :: json
     TYPE (json_value), POINTER, INTENT(IN) :: svalue
-    TYPE (json_core) :: json
     DOUBLE PRECISION :: thearea, theelev
     TYPE (simple_storage), POINTER :: s
-    CHARACTER (LEN=256) :: fld, msg
-    LOGICAL :: found
     INTEGER :: ierr
 
     ierr = 0
 
     NULLIFY(sptr%p)
 
-    CALL json%initialize()
-    IF (json%failed()) THEN
-       WRITE(msg, *) 'storage_factory_generate_simple: cannot initialize json'
-       CALL error_message(msg, fatal=.FALSE.)
-       ierr = ierr + 1
-       RETURN
-    END IF
-
-    fld = "Area"
-    CALL json%get(svalue, fld, thearea, found)
-    IF (json%failed()) THEN
-       WRITE(msg, *) 'storage_factory_generate_simple: JSON error looking for ', TRIM(fld)
-       CALL error_message(msg, fatal=.FALSE.)
-       ierr = ierr + 1
-    ELSE IF (.NOT. found) THEN
-       WRITE(msg, *) 'storage_factory_generate_simple: required value "', TRIM(fld), '" not found'
-       CALL error_message(msg, fatal=.FALSE.)
-       ierr = ierr + 1
-    END IF
-
-    fld = "BottomElevation"
-    CALL json%get(svalue, fld, theelev, found)
-    IF (json%failed()) THEN
-       WRITE(msg, *) 'storage_factory_generate_simple: JSON error looking for ', TRIM(fld)
-       CALL error_message(msg, fatal=.FALSE.)
-       ierr = ierr + 1
-    ELSE IF (.NOT. found) THEN
-       WRITE(msg, *) 'storage_factory_generate_simple: required value "', TRIM(fld), '" not found'
-       CALL error_message(msg, fatal=.FALSE.)
-       ierr = ierr + 1
-    END IF
+    ierr = ierr + &
+         &json_get_double(json, svalue, "Area", "storage_factory_generate_simple", thearea)
+    ierr = ierr + &
+         &json_get_double(json, svalue, "BottomElevation", "storage_factory_generate_simple", thearea)
 
     IF (ierr .EQ. 0) THEN
        ALLOCATE(s)
@@ -80,9 +95,98 @@ CONTAINS
        NULLIFY(s)
     END IF
 
-    CALL json%destroy()
-    
   END FUNCTION storage_factory_generate_simple
+
+  ! ----------------------------------------------------------------
+  !  FUNCTION storage_factory_generate_function
+  !
+  ! An example function storage JSON:
+  !
+  !  "Storage" : {
+  !      "type" : "function",
+  !      "Area" : {
+  !          "alpha" : 0.0,
+  !          "beta"  : 0.0,
+  !          "gamma" : 0.0
+  !      }
+  !      "Stage" : {
+  !          "alpha" : 0.0,
+  !          "beta"  : 0.0,
+  !          "gamma" : 0.0
+  !      }
+  !  }
+  !
+  ! ----------------------------------------------------------------
+  FUNCTION storage_factory_generate_function(this, json, svalue) RESULT (sptr)
+
+    IMPLICIT NONE
+
+    TYPE (storage_ptr) :: sptr
+    CLASS (storage_factory), INTENT(INOUT) :: this
+    TYPE (json_core), INTENT(INOUT) :: json
+    TYPE (json_value), POINTER, INTENT(IN) :: svalue
+
+    DOUBLE PRECISION :: alpha_a, beta_a, gamma_a
+    DOUBLE PRECISION :: alpha_h, beta_h, gamma_h
+
+    TYPE (function_storage), POINTER :: s
+    
+    CHARACTER (LEN=256) :: msg, fld
+    TYPE (json_value), POINTER :: jv
+    LOGICAL :: found
+    INTEGER :: ierr
+
+    ierr = 0
+
+    NULLIFY(sptr%p)
+
+    IF (json%failed()) THEN
+       WRITE(msg, *) 'storage_factory_generate_function: cannot initialize json'
+       CALL error_message(msg, fatal=.FALSE.)
+       ierr = ierr + 1
+       RETURN
+    END IF
+
+    fld = "Area"
+    CALL json%get(svalue, fld, jv, found)
+    IF (.NOT. found) THEN
+       WRITE(msg, *) 'storage_factory_generate_function: JSON error looking for "', TRIM(fld), '" not found'
+       CALL error_message(msg, fatal=.FALSE.)
+       ierr = ierr + 1
+    ELSE 
+       ierr = ierr + &
+            &json_get_double(json, svalue, "Alpha", "storage_factory_generate_function", alpha_a)
+       ierr = ierr + &
+            &json_get_double(json, svalue, "Beta", "storage_factory_generate_function", beta_a)
+       ierr = ierr + &
+            &json_get_double(json, svalue, "Gamma", "storage_factory_generate_function", gamma_a)
+    END IF
+
+    fld = "Stage"
+    CALL json%get(svalue, fld, jv, found)
+    IF (.NOT. found) THEN
+       WRITE(msg, *) 'storage_factory_generate_function: JSON error looking for "', TRIM(fld), '" not found'
+       CALL error_message(msg, fatal=.FALSE.)
+       ierr = ierr + 1
+    ELSE 
+       ierr = ierr + &
+            &json_get_double(json, svalue, "Alpha", "storage_factory_generate_function", alpha_h)
+       ierr = ierr + &
+            &json_get_double(json, svalue, "Beta", "storage_factory_generate_function", beta_h)
+       ierr = ierr + &
+            &json_get_double(json, svalue, "Gamma", "storage_factory_generate_function", gamma_h)
+    END IF
+
+    IF (ierr .EQ. 0) THEN
+       ALLOCATE(s)
+       s = function_storage(alpha_a, beta_a, gamma_a, &
+            &alpha_h, beta_h, gamma_h)
+       sptr%p => s
+       NULLIFY(s)
+    END IF
+
+  END FUNCTION storage_factory_generate_function
+
 
   
   ! ----------------------------------------------------------------
@@ -128,7 +232,9 @@ CONTAINS
     IF (ierr .EQ. 0) THEN
        SELECT CASE (tword)
        CASE ("simple")
-          sptr = this%generate_simple(svalue)
+          sptr = this%generate_simple(json, svalue)
+       CASE ("function")
+          sptr = this%generate_function(json, svalue)
        CASE DEFAULT
           WRITE(msg, *) 'storage_factory_generate: unknown type:"', TRIM(tword), '"'
           CALL error_message(msg, fatal=.FALSE.)
--- storage_module.f90
+++ storage_module.f90
@@ -7,7 +7,7 @@
 ! ----------------------------------------------------------------
 ! ----------------------------------------------------------------
 ! Created April  3, 2020 by William A. Perkins
-! Last Change: 2021-01-21 08:50:49 d3g096
+! Last Change: 2021-01-25 11:59:41 d3g096
 ! ----------------------------------------------------------------
 ! ----------------------------------------------------------------
 ! MODULE storage_module
@@ -78,6 +78,13 @@ MODULE storage_module
   END INTERFACE
 
   ! ----------------------------------------------------------------
+  ! TYPE storage_ptr
+  ! ----------------------------------------------------------------
+  TYPE, PUBLIC :: storage_ptr
+     CLASS (storage_t), POINTER :: p
+  END type storage_ptr
+
+  ! ----------------------------------------------------------------
   ! TYPE simple_storage
   ! A very simple storage defined by constant area and bottom elev
   ! ----------------------------------------------------------------
@@ -98,11 +105,32 @@ MODULE storage_module
   END INTERFACE simple_storage
 
   ! ----------------------------------------------------------------
-  ! TYPE storage_ptr
+  ! TYPE function_storage
+  !
+  ! Storage represented by general equations for area and WSE as
+  ! described by
+  !
+  ! Zhao, Gang, Huilin Gao, Bibi S. Naz, Shih-Chieh Kao, and Nathalie
+  ! Voisin. 2016.  Integrating a Reservoir Regulation Scheme into a
+  ! Spatially Distributed Hydrological Model.  Advances in Water
+  ! Resources 98 (December): 16
+  ! 31. https://doi.org/10.1016/j.advwatres.2016.10.014.
   ! ----------------------------------------------------------------
-  TYPE, PUBLIC :: storage_ptr
-     CLASS (storage_t), POINTER :: p
-  END type storage_ptr
+  TYPE, PUBLIC, EXTENDS(storage_t) :: function_storage
+     DOUBLE PRECISION :: alpha_a, beta_a, gamma_a
+     DOUBLE PRECISION :: alpha_h, beta_h, gamma_h
+   CONTAINS
+     PROCEDURE :: area => function_storage_area
+     PROCEDURE :: volume => function_storage_volume
+     PROCEDURE :: depth => function_storage_depth
+     PROCEDURE :: dvdy => function_storage_dvdy
+     PROCEDURE :: stage => function_storage_stage
+     PROCEDURE :: bottom => function_storage_bottom
+  END type function_storage
+
+  INTERFACE function_storage
+     MODULE PROCEDURE new_function_storage
+  END INTERFACE function_storage
 
 CONTAINS
 
@@ -232,6 +260,139 @@ CONTAINS
     b = this%ybottom
 
   END FUNCTION simple_storage_bottom
+
+  ! ----------------------------------------------------------------
+  !  FUNCTION new_function_storage
+  ! ----------------------------------------------------------------
+  FUNCTION new_function_storage(alpha_a, beta_a, gamma_a, &
+       & alpha_h, beta_h, gamma_h) RESULT(s)
+
+    IMPLICIT NONE
+    TYPE(function_storage) :: s
+    DOUBLE PRECISION, INTENT(IN) :: alpha_a, beta_a, gamma_a, alpha_h, beta_h, gamma_h
+
+    s%alpha_a = alpha_a
+    s%beta_a = beta_a
+    s%gamma_a = gamma_a
+
+    s%alpha_h = alpha_h
+    s%beta_h = beta_h
+    s%gamma_h = gamma_h
+
+    ! FIXME: need to do some checks:
+    !  - area > 0 @ S >= 0
+    !  - monotonically increasing: alpha > 0.0? 
+
+  END FUNCTION new_function_storage
+
+  ! ----------------------------------------------------------------
+  !  FUNCTION function_storage_area
+  ! ----------------------------------------------------------------
+  FUNCTION function_storage_area(this, y) RESULT(a)
+
+    IMPLICIT NONE
+    DOUBLE PRECISION :: a
+    CLASS (function_storage), INTENT(IN) :: this
+    DOUBLE PRECISION, INTENT(IN) :: y
+
+    DOUBLE PRECISION :: v
+
+    a = 0.0
+    v = this%volume(y)
+    IF (v .GT. 0.0) THEN
+       a = this%alpha_a*v**this%beta_a + this%gamma_a
+    END IF
+    
+  END FUNCTION function_storage_area
+
+
+  ! ----------------------------------------------------------------
+  !  FUNCTION function_storage_volume
+  ! ----------------------------------------------------------------
+  FUNCTION function_storage_volume(this, y) RESULT(v)
+
+    IMPLICIT NONE
+    DOUBLE PRECISION :: v
+    CLASS (function_storage), INTENT(IN) :: this
+    DOUBLE PRECISION, INTENT(IN) :: y
+
+    v = 0.0
+    IF (y .GT. this%gamma_h) THEN
+       v = exp((log(y - this%gamma_h) - log(this%alpha_h))/this%beta_h)
+    END IF
+  END FUNCTION function_storage_volume
+
+
+  ! ----------------------------------------------------------------
+  !  FUNCTION function_storage_depth
+  ! average depth = V/A
+  ! ----------------------------------------------------------------
+  FUNCTION function_storage_depth(this, y) RESULT(d)
+
+    IMPLICIT NONE
+    DOUBLE PRECISION :: d
+    CLASS (function_storage), INTENT(IN) :: this
+    DOUBLE PRECISION, INTENT(IN) :: y
+
+    DOUBLE PRECISION :: a, s
+
+    d = 0.0
+    IF (y .GT. this%bottom()) THEN
+       a = this%area(y)
+       s = this%volume(y)
+       d = s/a
+    END IF
+
+  END FUNCTION function_storage_depth
+
+
+  ! ----------------------------------------------------------------
+  !  FUNCTION function_storage_dvdy
+  ! ----------------------------------------------------------------
+  FUNCTION function_storage_dvdy(this, y) RESULT(dvdy)
+
+    IMPLICIT NONE
+
+    DOUBLE PRECISION :: dvdy
+    CLASS (function_storage), INTENT(IN) :: this
+    DOUBLE PRECISION, INTENT(IN) :: y
+    
+    dvdy = this%area(y)
+    
+  END FUNCTION function_storage_dvdy
+
+
+  ! ----------------------------------------------------------------
+  !  FUNCTION function_storage_stage
+  ! ----------------------------------------------------------------
+  FUNCTION function_storage_stage(this, v) RESULT (y)
+
+    IMPLICIT NONE
+    
+    DOUBLE PRECISION :: y
+    CLASS (function_storage), INTENT(IN) :: this
+    DOUBLE PRECISION, INTENT(IN) :: v
+
+    y = this%alpha_h*v**this%beta_h + this%gamma_h
+  
+  END FUNCTION function_storage_stage
+
+
+  ! ----------------------------------------------------------------
+  ! FUNCTION function_storage_bottom
+  !
+  ! WSE when storage is zero, presumably
+  ! ----------------------------------------------------------------
+  FUNCTION function_storage_bottom(this) RESULT (ybot)
+
+    IMPLICIT NONE
+    DOUBLE PRECISION :: ybot
+    CLASS (function_storage), INTENT(IN) :: this
+
+    ybot = this%gamma_h
+    
+  END FUNCTION function_storage_bottom
+
 
 
 END MODULE storage_module
